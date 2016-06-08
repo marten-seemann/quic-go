7c11552715af53ce0cf10444192fa7e8fa0886aa
Marten Seemann <martenseemann@gmail.com>
2016-06-08 15:06:45 +0700

    fix script

--- .travis/script.sh
+++ .travis/script.sh
@@ -4,9 +4,9 @@ set -e
 
 go get -t ./...
 if [ ${TESTMODE} == "unit" ]; then
-  ginkgo -r --cover --randomizeAllSpecs --randomizeSuites --trace --progress --skipPackage integrationtest
+  ginkgo -r --cover --randomizeAllSpecs --randomizeSuites --trace --progress --skipPackage integrationtests
 fi
 
 if [ ${TESTMODE} == "integration" ]; then
-  ginkgo -r --randomizeAllSpecs --randomizeSuites --trace --progress
+  ginkgo -r --randomizeAllSpecs --randomizeSuites --trace --progress integrationtests
 fi
